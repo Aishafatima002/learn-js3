82d7fa48dc8c4b45510807a791f2b0982e18c846
Aisha <fatimaaisha002@gmail.com>
2024-07-01 16:04:00 +0500

    new repo

---
+++ script.js
@@ -0,0 +1,95 @@
+//function
+// 1. take input n display output
+// function myfunction(){
+//     console.log("hello world")
+//     console.log("i love js")
+// }
+// myfunction()
+// myfunction()
+// myfunction()
+// myfunction()
+
+//2.
+// function myfunction(a,b) {//parameter
+//     console.log(a+b)
+// }
+// myfunction(5,7)
+// myfunction(17+45)
+// myfunction(1+"hello")
+// myfunction("24","13")//ayrgument
+
+//3.
+
+// function sum(y,x) {
+//     //local var or block scope
+//     s=y+x;
+//     return s
+// }
+// let value=sum(67,82);
+// console.log(value)
+
+//
+// function telltime(){
+// let new1=new Date();
+// let theHr=new1.getHours()
+// var theMin = new1.getMinutes();
+// alert("Current time: "+ theHr + ":" + theMin);
+// }
+// telltime()
+
+//
+// function multiplications(p1,p2) {
+//     return p1*p2
+// }
+// console.log(multiplications(4,3))
+
+//
+// function forenhietToCelceius(fahrenheit) {
+//     return 5/9*(fahrenheit-32);
+// }
+// console.log(forenhietToCelceius(45))
+
+//
+// let text="outside";
+// function myfunction(){
+//     let carName="honda"
+// }
+//
+// function myfunction(a){
+//  return a*(4)
+// }
+// // console.log(myfunction(2));
+// // console.log(myfunction(3))
+// console.log(myfunction(4))
+
+//factorial
+// function factorial(n){
+//     if(n===0 || n===1){
+//         return 1
+//     } else{
+//         return n* factorial(n-1);
+//     }
+    
+// }
+// console.log(factorial(1))
+// console.log(factorial(2))
+// console.log(factorial(3))
+// console.log(factorial(4))
+// console.log(factorial(5))
+// console.log(factorial(6))
+// console.log(factorial(7))
+// console.log(factorial(8))
+// console.log(factorial(9))
+// console.log(factorial(10))
+
+// arrow function is used for small codings but similar to functions
+// const additionOfNum=(a,b)=>{
+//  return a+b
+// }
+// console.log(additionOfNum(2,6))
+
+//
+// const hello=()=>{
+//     return "i love js";
+// }
+// console.log(hello())
