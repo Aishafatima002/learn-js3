40917899c540340a8efb978237c950fcadfc8bcb
Aisha <fatimaaisha002@gmail.com>
2024-07-03 16:11:51 +0500

    up

--- script.js
+++ script.js
@@ -93,3 +93,101 @@
 //     return "i love js";
 // }
 // console.log(hello())
+// class activity
+//q1
+// function myfunction(){
+//   const dt=new Date();
+//   document.write(dt)
+// }
+// myfunction()
+
+//q2
+// let firstName=prompt("Enter your first name");
+// let lastName=prompt("Enter your last name");
+// function greet(firstName,lastName){
+//     document.write("welcome"+" "+firstName+ " "+lastName)
+// }
+// greet(firstName,lastName)
+
+// let num1=+prompt("Enter first no:")
+// let num2=+prompt("Enter last no: ");
+// function ans(num1,num2){
+// return num1+num2
+// }
+// console.log(ans(num1,num2))
+
+
+// make a smal calculator
+// let num1=+prompt("Enter num1");
+// let num2=+prompt("Enter num2");
+// let operator=prompt("enter your operator");
+// function myfunction(num1, num2, operator){
+//     if(operator==="+"){
+//         document.write(num1 +""+operator +num2 + ""+"="+ (num1+num2))  
+//     }
+//     else if(operator==="-"){
+//         document.write(num1+""+operator +" "+num2+""+"="+ (num1-num2))
+//     }
+//     else if(operator==="*"){
+//         document.write(num1+""+ operator+num2+""+"="+ (num1*num2))
+//     }
+//     else if(operator==="%"){
+//         document.write(num1+""+ operator+num2+""+"="+ (num1%num2))
+//     }
+// }
+// myfunction(num1,num2,operator)
+//q4
+// function sqr(a){
+//     return a*a
+// }
+// console.log(sqr(2))
+
+//q5
+//part1
+// function areaOfRectangle(width,hieght){
+//    return width*hieght
+// }
+// console.log(areaOfRectangle(32,12));
+// //part2
+// let w=4
+// let h=5
+
+// function myfunction(w,h){
+//     return w*h
+// }
+// console.log(myfunction(w,h))
+
+//q6
+// function TheCircumferenceOfCircle(radius) {
+//    let r=6
+//     return radius=2*3.14*r
+// }
+// console.log(TheCircumferenceOfCircle())
+
+// function AreaOfCircle(radius) {
+//     let r=3
+//     return radius=3.14*(r*r)
+// }
+// console.log(AreaOfCircle())
+
+// let startNum=+prompt("Enter your sart number");
+// let endNum=+prompt("Enter your end number");
+// function count(startNum,endNum) {
+//     if(startNum>endNum) {
+//         for(let i = startNum; i>endNum; i--) {
+//             document.write(i+"<br>")
+//         } 
+//         }
+//     else if(startNum<endNum){
+//         for(let i = startNum; i < endNum; i++) {
+//             document.write(i+"<br>");
+            
+//         }
+//     }
+
+//     else if(startNum === endNum){
+//         document.write("both are equal")
+//     }
+// }
+// count(startNum,endNum)
+
